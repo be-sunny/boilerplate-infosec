7b7861b1154a9a290b0c50cee1967956d771c117
be Sunny <be.sunny.code@gmail.com>
2024-05-13 09:25:51 +0000

    feat: reformat

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({
-  action: 'deny'
-}));
+app.use(helmet.frameguard(
+  {action: 'deny'}
+));
 
 
 
