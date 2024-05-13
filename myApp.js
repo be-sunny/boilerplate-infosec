21a5cf2e4daf019b294e27ade92ed6ee8aca3b85
be Sunny <be.sunny.code@gmail.com>
2024-05-13 09:24:27 +0000

    feat: added frameguard

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({
+  action: 'deny'
+}));
 
 
 
